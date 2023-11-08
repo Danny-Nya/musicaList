a25e5a85e4b7cae9bd50e5af10f86429bfe0c538
Danny-Nya <82296574+Danny-Nya@users.noreply.github.com>
2023-11-07 19:11:18 -0500

    Cross Origins

--- src/main/java/com/example/musicalist/controllers/RegistroController.java
+++ src/main/java/com/example/musicalist/controllers/RegistroController.java
@@ -3,6 +3,7 @@ package com.example.musicalist.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.example.musicalist.respositories.UsuarioVotanteRepository;
 
 @Controller
 @RequestMapping("/registro")
+@CrossOrigin
 public class RegistroController {
 
     @Autowired
